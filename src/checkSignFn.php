56e481aefcaa4a1606656299da7e21ab20e9f2dd
Alex W3O <alex@w3o.com>
2024-04-14 22:15:55 +0300

    fix autoload path

--- src/checkSignFn.php
+++ src/checkSignFn.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!\function_exists('sodium_crypto_sign_verify_detached')) {
-    require '../vendor/autoload.php';
+    require \dirname(__DIR__) . '/vendor/autoload.php';
     if (!\function_exists('sodium_crypto_sign_verify_detached')) {
         die("Function 'sodium_crypto_sign_verify_detached' not found, please enable 'sodium' ext. or install polyfill, for example 'paragonie/sodium_compat'");
     }
