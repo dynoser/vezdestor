2e27bede6e40271815af018e097f46161652ba79
Alex W3O <alex@w3o.com>
2024-04-15 06:47:25 +0300

    added 500 http code

--- src/checkSignFn.php
+++ src/checkSignFn.php
@@ -3,6 +3,8 @@
 if (!\function_exists('sodium_crypto_sign_verify_detached')) {
     require \dirname(__DIR__) . '/vendor/autoload.php';
     if (!\function_exists('sodium_crypto_sign_verify_detached')) {
-        die("Function 'sodium_crypto_sign_verify_detached' not found, please enable 'sodium' ext. or install polyfill, for example 'paragonie/sodium_compat'");
+        http_response_code(500);
+        echo "Function 'sodium_crypto_sign_verify_detached' not found, please enable extension = sodium or install polyfill (for example, composer require paragonie/sodium_compat)\n";
+        die;
     }
 }
