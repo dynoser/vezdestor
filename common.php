f1bbdb97c228bc15a799e95b0a9ec02edef37312
Alex W3O <alex@w3o.com>
2024-04-17 07:15:39 +0300

    added historyPath

--- common.php
+++ common.php
@@ -15,11 +15,13 @@ $accName = \trim(\substr($path, 0, $i), '/');
 $inAccPath = \substr($path, $i);
 
 //the folder in which the files will be stored
-$storagePath = __DIR__ . '/storage';
+$storagePath = \strtr(__DIR__, '\\', '/') . '/storage';
+$historyPath = $storagePath . '/history';
+$historyOn = true;
 $accPath = $storagePath . '/' . $accName;
 
 $upubs = [
-    //account name (folder in storage) => public key
+    //account name (folder in storage) => public key (ed25519-sign in base64u)
     'dynoser' => 'aOk1rVVhWoaYZzThCNWiaBMGeaQMJ_hAZT-HTGfZkKY'
 ];
 // maximum allowed file size
